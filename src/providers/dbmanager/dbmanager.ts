844831b7c3d3668443855593fcb4167db3e8fd5a
Felipe Ramirez <framirez.mill@gmail.com>
2018-05-09 20:39:35 -0500

    Temp fix 2

--- src/providers/dbmanager/dbmanager.ts
+++ src/providers/dbmanager/dbmanager.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {SQLite, SQLiteObject} from '@ionic-native/sqlite';
 import {Point} from "../../models/point.model";
+import {ToastController} from "ionic-angular";
 
 
 @Injectable()
 export class DbmanagerProvider {
 
-  constructor(private sqlite: SQLite) {
+  constructor(private sqlite: SQLite, private _tc: ToastController ) {
   }
 
   getAllPoints() {
@@ -38,18 +39,29 @@ export class DbmanagerProvider {
         db.executeSql('INSERT INTO point VALUES(?, ?, ?, NULL, ?)', [point.name, point.description, point.date, point.type])
           .then(res => {
             console.log(res);
+            this.presentToast(res, 3000);
             resolve(true);
           }).catch(err => {
             console.log('peguelo fallo');
+            this.presentToast(err, 3000);
             reject();
           });
       }).catch(err => {
         console.log(err);
+        this.presentToast(err, 3000);
         reject();
       });
     });
 
   }
 
+  presentToast(message: string, duration: number) {
+    let toast = this._tc.create({
+      message: message,
+      duration: duration,
+      position: 'bottom'
+    });
+    toast.present();
+  }
 
 }
